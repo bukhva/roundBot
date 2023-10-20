d77bad1317f3ceb2761aac0f054b1e30f1a17722
bukhva <vivanv345672176@mail.ru>
2023-10-21 00:57:06 +0300

    to build

--- bot.py
+++ bot.py
@@ -1,17 +1,32 @@
-from aiogram import Bot, Dispatcher, types, executor
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import FSInputFile
 
 token_api = "6565587911:AAGG80wsIafXZdshqZXwkLuNWfXk735rmkk"
-
 bot = Bot(token=token_api)
 dp = Dispatcher(bot=bot)
 
 
-@dp.message_handler(content_types=["video"])
 async def download_video(message: types.Message):
     file_id = message.video.file_id # Get file id
     file = await bot.get_file(file_id) # Get file path
     print(file_id, file, sep = "\n")
     await bot.download_file(file.file_path, f"media/{file.file_unique_id}.mp4") # Download video and save output in file "video.mp4"
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+@dp.message()
+async def send_video(message):
+    video_note = FSInputFile("output.mp4")
+    await bot.send_video_note(message.chat.id, video_note)
+
+
+
+async def main():
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
---
+++ editor.py
@@ -0,0 +1,2 @@
+#cmprsr: to sqr
+# ffmpeg -i SqOutput.mp4 -vf scale=384:384,setsar=1:1 output.mp4
