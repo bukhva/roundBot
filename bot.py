aafc3192c0b420281314d6f1454719ad2a143062
bukhva <vivanv345672176@mail.ru>
2023-10-22 22:08:59 +0300

    it works! but it cannot be upload to PAW

--- bot.py
+++ bot.py
@@ -1,23 +1,39 @@
 import asyncio
 import logging
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import FSInputFile
+from editor import video_to_video_note
+from os import remove
 
 token_api = "6565587911:AAGG80wsIafXZdshqZXwkLuNWfXk735rmkk"
 bot = Bot(token=token_api)
 dp = Dispatcher(bot=bot)
 
 
-async def download_video(message: types.Message):
-    file_id = message.video.file_id # Get file id
-    file = await bot.get_file(file_id) # Get file path
-    print(file_id, file, sep = "\n")
-    await bot.download_file(file.file_path, f"media/{file.file_unique_id}.mp4") # Download video and save output in file "video.mp4"
-
 @dp.message()
-async def send_video(message):
-    video_note = FSInputFile("output.mp4")
-    await bot.send_video_note(message.chat.id, video_note)
+async def download_video(message: types.Message):
+    file_id = message.video.file_id
+    file = await bot.get_file(file_id)
+    path = f"media/{file.file_unique_id}.mp4"
+    await bot.download_file(file.file_path, path) # Download video and save output in file "video.mp4"
+    await message.reply("Получили! Обрабатываем...")
+    await send_video(message, file.file_unique_id)
+
+
+async def send_video(message, videoId):
+    path = f"media/{videoId}.mp4"
+    newPath = f"media/result{videoId}.mp4"
+
+    if(video_to_video_note(path, newPath)):
+        video_note = FSInputFile(newPath)
+        await bot.send_video_note(message.chat.id, video_note)
+        #remove_files(path, newPath)
+
+
+def remove_files(path, newPath):
+    remove(path)
+    remove(newPath)
 
 
 
--- editor.py
+++ editor.py
@@ -1,2 +1,19 @@
-#cmprsr: to sqr
-# ffmpeg -i SqOutput.mp4 -vf scale=384:384,setsar=1:1 output.mp4
+import os
+import ffmpeg
+
+def video_to_video_note(floc, nfloc):
+    probe = ffmpeg.probe(floc)
+    w=probe["streams"][0]['width']
+    h=probe["streams"][0]['height']
+    s = min(w,h)
+    if w>h:
+        x=(w-h)//2
+        y=0
+    if w==h:
+        x=0
+        y=0
+    if w<h:
+        x=0
+        y=(h-w)//2
+    os.system(f"ffmpeg -y -i {floc} -vf crop={s}:{s}:{x}:{y},scale=384:384,setsar=1:1 {nfloc}")
+    return True
